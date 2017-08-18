bcd13cbc59364197808a4cf0489702e4d73734e3
1001 <dtssipl@gmail.com>
2017-08-18 13:11:27 +0530

    first Movie

---
+++ MvcStoredProcedureSample/MvcStoredProcedureSample/Models/MovieDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MvcStoredProcedureSample.Models
+{
+    public class MovieDbContext : DbContext
+    {
+        
+        public MovieDbContext() : base("name=MovieDbContext")
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {  
+            modelBuilder.Entity<Movie>().MapToStoredProcedures();  
+        }
+
+}
+}
